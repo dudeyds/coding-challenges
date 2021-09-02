46a3ea15a2f8a0ffc6d2a45fd340faeec0495ff2
James Morris <james@educater.co.uk>
2021-09-02 11:28:15 +0100

    Initial Commit - setting up project

---
+++ Supermarket Kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Supermarket_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
